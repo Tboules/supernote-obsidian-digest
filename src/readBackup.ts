45b47f05c6da6cd7ed355ae25bd4dd37230b269a
Tboules <tboules@gmail.com>
2026-06-05 18:29:37 -0700

    reading digests

--- src/readBackup.ts
+++ src/readBackup.ts
@@ -6,12 +6,26 @@ export default async function readBackup(path: string) {
 	const buffer = fs.readFileSync(path);
 	const zip = await JSZip.loadAsync(buffer);
 
+	console.log(zip.forEach((p, f) => console.log({ p, f })));
+
 	const knowledgeFile = JSON.parse(
 		(await zip.file("backup/DIGEST/knowledge.json")?.async("string")) ??
 			"[]",
 	);
 
 	console.log(knowledgeFile);
+
+	knowledgeFile.forEach(async (knowledge: any) => {
+		console.log(knowledge.commentHandwriteName);
+
+		const markPath = "backup/DIGEST/handwrite/";
+
+		const markFile = await zip.file(
+			markPath + knowledge.commentHandwriteName,
+		);
+
+		console.log(markFile);
+	});
 
 	return path;
 }
